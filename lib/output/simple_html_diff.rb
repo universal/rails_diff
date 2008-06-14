c3af01ce96a4d1ae51b97e0805b7b08af0050d4b
universal <jhed@informatik.uni-kiel.de>
2008-06-14 14:15:40 +0200

    added html escape to the provided output generator

--- lib/output/simple_html_diff.rb
+++ lib/output/simple_html_diff.rb
@@ -1,4 +1,7 @@
+require 'action_view/template_handlers/erb'
 class Output::SimpleHtmlDiff #:nodoc:
+  include ERB::Util
+
   attr_accessor :content
   attr_accessor :old
   attr_accessor :new
@@ -10,27 +13,27 @@ class Output::SimpleHtmlDiff #:nodoc:
   end
     
   def change(event)
-    @content << %Q|<tr><td>#{old}. </td><td><pre class="only_a">#{event.old_element}</pre></td><td><pre class="only_b">#{event.new_element}</pre></td><td>#{new}. </td></tr>\n|
+    @content << %Q|<tr><td>#{old}. </td><td><pre class="only_a">#{html_escape(event.old_element)}</pre></td><td><pre class="only_b">#{html_escape(event.new_element)}</pre></td><td>#{new}. </td></tr>\n|
     @old += 1
     @new += 1
   end
 
     # This will be called with both lines are the same
   def match(event)
-    @content << %Q|<tr><td>#{old}. </td><td colspan="2"><pre class="match">#{event.old_element}</pre></td><td>#{new}. </td></tr>\n|
+    @content << %Q|<tr><td>#{old}. </td><td colspan="2"><pre class="match">#{html_escape(event.old_element)}</pre></td><td>#{new}. </td></tr>\n|
     @old += 1
     @new += 1
   end
 
     # This will be called when there is a line in A that isn't in B
   def discard_a(event)
-    @content << %Q|<tr><td>#{old}. </td><td><pre class="only_a">#{event.old_element}</pre></td><td></td><td></td></tr>\n|
+    @content << %Q|<tr><td>#{old}. </td><td><pre class="only_a">#{html_escape(event.old_element)}</pre></td><td></td><td></td></tr>\n|
     @old += 1
   end
 
     # This will be called when there is a line in B that isn't in A
   def discard_b(event)
-    @content << %Q|<tr><td></td><td></td><td><pre class="only_b">#{event.new_element}</pre></td><td>#{new}. </td></tr>\n|
+    @content << %Q|<tr><td></td><td></td><td><pre class="only_b">#{html_escape(event.new_element)}</pre></td><td>#{new}. </td></tr>\n|
     @new += 1
   end
 end
