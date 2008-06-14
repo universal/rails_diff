b8a60e4d602b4d94b8281914f817c20fbc094f41
universal <jhed@informatik.uni-kiel.de>
2008-06-14 13:57:53 +0200

    cleaned up plugin code a bit, started commenting ;) added description and added gem dependency to init.rb

--- init.rb
+++ init.rb
@@ -1,1 +1,8 @@
-# Include hook code here
+config.gem "diff-lcs", :lib => "diff/lcs"
+require 'rails_diff'
+config.after_initialize do
+  # Include our helper into every view
+  ActionView::Base.send :include, DiffHelper
+end
+
+
--- lib/rails_diff.rb
+++ lib/rails_diff.rb
@@ -1,63 +1,15 @@
-begin
-  require 'rubygems'
-  require 'diff/lcs'
-  require 'diff/lcs/string'
-  require 'text/format'
-rescue
-  raise "You need to have RubyGems (http://www.rubygems.org/) and ... installed"
-end
-
 class RailsDiff
-  def self.diff(old, new)
-    hd = RailsDiff::HtmlDiff.new
-    tf = Text::Format.new
-    tf.tabstop = 2
-    preprocess = lambda { |line| tf.expand(line.chomp) }
+  def self.diff(old, new, output = Output::SimpleHtmlDiff)
+    out = output.new
     
+    # split strings into lines array
     a = old.split(/\n|\r\n/)
     b = new.split(/\n|\r\n/)
+    #remove trailing.. whitespace
+    #a.map{|c| c.strip!}
+    #b.map{|c| c.strip!}
     
-    a.map(&preprocess)
-    b.map(&preprocess)
-    Diff::LCS.traverse_balanced(a, b, hd)
-#    Diff::LCS.traverse_sequences(a, b, hd)
-    hd.custom
-  end
-end
-
-class RailsDiff::HtmlDiff #:nodoc:
-  attr_accessor :custom
-  attr_accessor :old
-  attr_accessor :new
-
-  def initialize
-    @custom = ""
-    @old = 1
-    @new = 1
-  end
-    
-  def change(event)
-    @custom << %Q|<tr><td>#{old}. </td><td><pre class="only_a">#{event.old_element}</pre></td><td><pre class="only_b">#{event.new_element}</pre></td><td>#{new}. </td></tr>\n|
-    @old += 1
-    @new += 1
-  end
-
-    # This will be called with both lines are the same
-  def match(event)
-    @custom << %Q|<tr><td>#{old}. </td><td colspan="2"><pre class="match">#{event.old_element}</pre>\n</td><td>#{new}. </td></tr>|
-    @old += 1
-    @new += 1
-  end
-
-    # This will be called when there is a line in A that isn't in B
-  def discard_a(event)
-    @custom << %Q|<tr><td>#{old}. </td><td><pre class="only_a">#{event.old_element}</pre></td><td></td><td></td></tr>\n|
-    @old += 1
-  end
-
-    # This will be called when there is a line in B that isn't in A
-  def discard_b(event)
-    @custom << %Q|<tr><td></td><td></td><td><pre class="only_b">#{event.new_element}</pre></td><td>#{new}. </td></tr>\n|
-    @new += 1
+    Diff::LCS.traverse_balanced(a, b, out)
+    out.content
   end
 end
