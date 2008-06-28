7f3d17e8cc93b827b98b33d0ac5a3bcea02221a7
universal <jhed@informatik.uni-kiel.de>
2008-06-28 18:44:57 +0200

    fixxed tags not being closed correctly

--- lib/output/simple_correction_diff.rb
+++ lib/output/simple_correction_diff.rb
@@ -11,23 +11,21 @@ class Output::SimpleCorrectionDiff #:nodoc:
   def change(event)
     out = Output::StringSimpleCorrectionDiff.new
     Diff::LCS.traverse_balanced(event.old_element, event.new_element, out)
-
-#    @content << %Q|<pre class="change">#{html_escape(event.new_element)}</pre><br/>\n|
-    @content << %Q|<pre class="change">#{out.content}</span></pre><br/>\n|
+    @content << %Q|<pre class="change">#{out.content}#{out.closing}</span></pre>\n|
   end
 
     # This will be called with both lines are the same
   def match(event)
-    @content << %Q|<pre class="match">#{html_escape(event.new_element)}</pre><br/>\n|
+    @content << %Q|<pre class="match">#{event.new_element}</pre>\n|
   end
 
     # This will be called when there is a line in A that isn't in B
   def discard_a(event)
-    @content << %Q|<pre class="only_a"><del>#{html_escape(event.old_element)}</del></pre><br/>\n|
+    @content << %Q|<pre class="only_a"><del>#{event.old_element}</del></pre>\n|
   end
 
     # This will be called when there is a line in B that isn't in A
   def discard_b(event)
-    @content << %Q|<pre class="only_b"><ins>#{html_escape(event.new_element)}</ins></pre><br/>\n|
+    @content << %Q|<pre class="only_b"><ins>#{event.new_element}</ins></pre>\n|
   end
 end
--- lib/rails_diff.rb
+++ lib/rails_diff.rb
@@ -1,8 +1,11 @@
+require 'action_view/template_handlers/erb'
 class RailsDiff
+  # add html_escape to class level ;)
+  extend ERB::Util
   def self.line_diff(old, new, output = Output::SimpleHtmlDiff)
     # split strings into lines array
-    a = old.split(/\n|\r\n/)
-    b = new.split(/\n|\r\n/)
+    a = html_escape(old).split(/\015?\012/)
+    b = html_escape(new).split(/\015?\012/)
     #remove trailing.. whitespace
     #a.map{|c| c.strip!}
     #b.map{|c| c.strip!}
@@ -12,8 +15,8 @@ class RailsDiff
   end
   
   def self.correction_diff(old, new, output = Output::SimpleCorrectionDiff)
-    a = old.split(/\n|\r\n/)
-    b = new.split(/\n|\r\n/)
+    a = html_escape(old).split(/\015?\012/)
+    b = html_escape(new).split(/\015?\012/)
     out = output.new
     Diff::LCS.traverse_balanced(a, b, out)
     out.content
